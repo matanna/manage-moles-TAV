6d35801794285d751ffdc849198d76778379d133
matanna <mat.bonhommeau85@gmail.com>
2021-05-31 22:10:13 +0200

    dev javascript for display mole cu when clik on the eye icon

--- public/js/display-moles-cu.js
+++ public/js/display-moles-cu.js
@@ -1,7 +1,25 @@
-const details = document.getElementsByClassName("details");
+//When the page is loading
+window.onload = function() {
 
-for(let i; i < details.length; i++) {
-    details[i].addEventListener('click', function(){
-        alert(this.id);
-    });
+    //We retrieve all elements with class "details"
+    const details = document.getElementsByClassName("details");
+
+    //For each "details", we listen click event for retrieve his id
+    for(let i = 0; i < details.length; i++) {
+        details[i].addEventListener('click', function(){
+            let id = details[i].getAttribute('id');
+
+            //We replace the word "button" in id by "display" for display the div with moles
+            id = id.replace('button', 'display');
+            
+            if (document.getElementById(id).getAttribute("hidden")) {
+                document.getElementById(id).removeAttribute("hidden"); 
+            } else {
+                document.getElementById(id).setAttribute("hidden", ""); 
+            }
+
+            
+
+        });
+    }
 }
